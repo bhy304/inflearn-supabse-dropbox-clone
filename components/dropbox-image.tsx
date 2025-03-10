e43b95a50d02785e285e356d61449d85d706cd3f
Hayeon Baek <bhy0512@gmail.com>
2025-03-10 22:47:34 +0900

    Fix: the issue of button position changes when clicked

--- components/dropbox-image.tsx
+++ components/dropbox-image.tsx
@@ -16,11 +16,13 @@ export default function DropboxImage() {
       {/* FileName */}
       <div>cutedog.jpeg</div>
       {/* Delete Image Button */}
-      <IconButton
-        variant='text'
-        className='absolute top-4 right-4 rounded-sm hover:bg-[#1DA1F2] focus:bg-[#1DA1F2] active:bg-[#1DA1F2]'>
-        <i className='fas fa-trash text-md text-white' />
-      </IconButton>
+      <div className='absolute top-4 right-4'>
+        <IconButton
+          variant='text'
+          className='rounded-sm hover:bg-[#1DA1F2] focus:bg-[#1DA1F2] active:bg-[#1DA1F2]'>
+          <i className='fas fa-trash text-md text-white' />
+        </IconButton>
+      </div>
     </div>
   )
 }
