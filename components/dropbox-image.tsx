a64c55e821f92f842826def91b02ec6c967ee7b9
Hayeon Baek <bhy0512@gmail.com>
2025-03-12 17:27:48 +0900

    Feat: Implement delete file

--- components/dropbox-image.tsx
+++ components/dropbox-image.tsx
@@ -1,10 +1,22 @@
 'use client'
 
-import { IconButton } from '@material-tailwind/react'
+import { IconButton, Spinner } from '@material-tailwind/react'
+import { useMutation } from '@tanstack/react-query'
+import { deleteFile } from 'actions/storage-actions'
+import { queryClient } from 'config/ReactQueryClientProvider'
 import { getImageURL } from 'utils/supabase/storage'
 
 export default function DropboxImage(props) {
   const { name } = props
+
+  const deleteFileMutaion = useMutation({
+    mutationFn: deleteFile,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['images'],
+      })
+    },
+  })
 
   return (
     <div className='relative w-full flex flex-col gap-2 p-4 border border-gray-100 rounded-2xl shadow-md'>
@@ -22,8 +34,15 @@ export default function DropboxImage(props) {
       <div className='absolute top-4 right-4'>
         <IconButton
           variant='text'
-          className='rounded-sm hover:bg-[#1DA1F2] focus:bg-[#1DA1F2] active:bg-[#1DA1F2]'>
-          <i className='fas fa-trash text-md text-white' />
+          className='rounded-sm hover:bg-[#1DA1F2] focus:bg-[#1DA1F2] active:bg-[#1DA1F2]'
+          onClick={() => {
+            deleteFileMutaion.mutate(name)
+          }}>
+          {deleteFileMutaion.isPending ? (
+            <Spinner />
+          ) : (
+            <i className='fas fa-trash text-md text-white' />
+          )}
         </IconButton>
       </div>
     </div>
